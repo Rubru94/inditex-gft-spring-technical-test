aaa5a12cfaded742cb63abefa6601e82398648b9
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2025-04-08 21:16:47 +0200

    feat: Add domain layer

---
+++ src/main/java/com/inditex/technical_test/price/domain/Price.java
@@ -0,0 +1,49 @@
+package com.inditex.technical_test.price.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "PRICES")
+public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "BRAND_ID")
+    private Integer brandId;
+
+    @Column(name = "START_DATE")
+    private LocalDateTime startDate;
+
+    @Column(name = "END_DATE")
+    private LocalDateTime endDate;
+
+    @Column(name = "PRICE_LIST")
+    private Integer priceList;
+
+    @Column(name = "PRODUCT_ID")
+    private Integer productId;
+
+    @Column(name = "PRIORITY")
+    private Integer priority;
+
+    @Column(name = "PRICE")
+    private Double price;
+
+    @Column(name = "CURR")
+    private String currency;
+}
